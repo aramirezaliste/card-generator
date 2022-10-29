64701d4db8ee5e0e387d30de0c2b3fdb66cd5a6a
aramirezaliste <aramirezaliste@gmail.com>
2022-10-29 14:53:57 +0000

    Segundo avance carta aleatoria

--- src/app.js
+++ src/app.js
@@ -8,4 +8,44 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let numeros = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1"
+  ];
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  let randomNumeros = numeros[Math.floor(Math.random() * (numeros.length - 1))];
+  let randomPalos = palos[Math.floor(Math.random() * (palos.length - 1))];
+
+  //console.log(randomPalos + randomNumeros);
+
+  let cambioPalo = document.querySelector(".iconoUno");
+  cambioPalo.innerHTML = randomPalos;
+
+  let cambioPaloDos = document.querySelector(".iconoDos");
+  cambioPaloDos.innerHTML = randomPalos;
+
+  let cambioNumero = document.querySelector(".textoCentral");
+  cambioNumero.innerHTML = randomNumeros;
+
+  if (randomPalos == "♦" || randomPalos == "♥") {
+    //console.log(randomPalos);
+    let cambioColorPalo = document.querySelector(".iconoUno");
+    cambioColorPalo.classList.add("red");
+    let cambioColorPaloDos = document.querySelector(".iconoDos");
+    cambioColorPaloDos.classList.add("red");
+  }
 };
